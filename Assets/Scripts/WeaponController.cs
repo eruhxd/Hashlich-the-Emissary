c4684cd558e8590d65121627db719ace0f95e4c7
eruhxd <asdrubal.banezr@estudiantes.arcos.cl>
2024-11-13 12:23:34 -0300

    0: Initial commit
    Assets iniciales de NOT!Rockman2, se iran removiendo progresivamente.
    
    Controller, dos armas iguales (por ahora) como prefab, camara, fisicas y semejantes listos; se le ira sumando de todo sobre la marcha.

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    [Header("References")]
+    public Transform weaponMuzzle;
+
+    [Header("General")]
+    public LayerMask hittableLayers;
+    public GameObject bulletHolePrefab;
+
+    [Header("Shoot Paramaters")]
+    public float fireRange = 200;
+    public float recoilForce = 4f; //Fuerza de retroceso del arma
+
+    [Header("Sounds & Visuals")]
+    public GameObject flashEffect;
+
+    private Transform cameraPlayerTransform;
+    Vector3 initialPosition;
+
+    private void Start()
+    {
+        initialPosition = this.transform.localPosition;
+        cameraPlayerTransform = GameObject.FindGameObjectWithTag("MainCamera").transform;
+    }
+
+    private void Update()
+    {
+        HandleShoot();
+
+        transform.localPosition = Vector3.Lerp(initialPosition, Vector3.zero, Time.deltaTime * 5f);
+    }
+
+    private void HandleShoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject flashClone = Instantiate(flashEffect, weaponMuzzle.position, Quaternion.Euler(weaponMuzzle.forward), transform);
+            Destroy(flashClone, 1f);
+
+            AddRecoil();
+
+            RaycastHit hit;
+            if (Physics.Raycast(cameraPlayerTransform.position, cameraPlayerTransform.forward, out hit, fireRange, hittableLayers))
+            {
+                GameObject bulletHoleClone = Instantiate(bulletHolePrefab, hit.point + hit.normal * 0.001f, Quaternion.LookRotation(hit.normal));
+                Destroy(bulletHoleClone, 4f);
+            }
+        }
+    }
+
+    private void AddRecoil()
+    {
+        transform.Rotate(-recoilForce, 0f, 0f);
+        transform.position = transform.position - transform.forward * (recoilForce / 50f);
+    }
+}
